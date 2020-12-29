c8cb5439ecae2da2bff55165b7606e46626fd7a5
leochastagnac <leochastagnac@gmail.com>
2020-12-29 17:00:17 +0100

    Animationstext

--- index.php
+++ index.php
@@ -1,148 +1,237 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+
 <html>
-    <head>
-        <title>TODO supply a title</title>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="css/style.css" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js"></script>
-    </head>
-    <body>
-        <section id="sec-1">
-            <div class="container">
-                <div class="imgMove1" data-rellax-speed="2">
-                    <img src="assets/images/icons8-linux-100.png" class="img1" alt="imagedeco"/>    
-                    <img src="assets/images/icon2.jpg" class="img2" alt="imagedeco"/> 
-                </div>
 
-                <div class="logo">
-                    <img src="assets/images/logo.png" alt="logo" />
-                </div>
-                <div class="slogan">
-                    <h1>De la recherche </h1><p>au</p><h1>Développement.</h1>
-                </div>
-                <div class="imgMove2" data-rellax-speed="8">
-                    <img src="assets/images/icon3.jpg" class="img3" alt="imagedeco"/>    
-                    <img src="assets/images/icon4.jpg" class="img4" alt="imagedeco"/>   
-                </div>
+<head>
+    <title>Chastagnac Léo</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/style.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js"></script>
+    <script src="https://unpkg.com/scrollreveal"></script>
+</head>
+
+<body>
+    <section id="sec-1">
+        <div class="container">
+            <div class="imgMove1" data-rellax-speed="2">
+                <img src="assets/images/icons8-linux-100.png" class="img1" alt="imagedeco" />
+                <img src="assets/images/icon2.jpg" class="img2" alt="imagedeco" />
             </div>
-        </section>
-        <script>
-            var rellax = new Rellax('.imgMove2');
-            var rellax = new Rellax('.imgMove1');
-        </script>
 
+            <div class="logo">
+                <img src="assets/images/logo.png" alt="logo" />
+            </div>
+            <div class="slogan">
+                <h1>De la recherche </h1>
+                <p>au</p>
+                <h1>Développement.</h1>
+            </div>
+            <div class="imgMove2" data-rellax-speed="8">
+                <img src="assets/images/icon3.jpg" class="img3" alt="imagedeco" />
+                <img src="assets/images/icon4.jpg" class="img4" alt="imagedeco" />
+            </div>
+        </div>
+    </section>
+    <section id="sec-2">
+        <div class="container">
+            <img src="assets/images/rubanwave.png" class="image__img" alt="banière wave" />
+            <div class="image__overlay">
+                <h1 class="title">Qui suis-je ?</h1>
+                <div class="texte">
+                    <p>Je m'appelle Léo Chastagnac, j'ai 19 ans, j'habite au Beausset et je suis passionné par l'informatique et les différentes technologies qui composent ce domaine.
+                        De plus je pratique la
+                        guitare en autodidacte depuis 5ans, le piano depuis 2ans et le
+                        développement personnel. Je également passionné de sport ou je
+                        pratique la musculation, le street workout, le tricking et le VTT</p>
+                </div>
 
-
-
-        <section id="sec-2">
-            <div class="container">
-                <img src="assets/images/rubanwave.png" class="image__img" alt="banière wave"/>
-                <div class="image__overlay">
-                    <h1 class="title">Qui suis-je ?</h1>
-                    <div class="texte">
-                        <p>Je m'appelle Léo Chastagnac, j'ai 19 ans, j'habite au Beausset et je suis passionné par l'informatique et les différentes technologies qui composent ce domaine.
-                            De plus je pratique la 
-                            guitare en autodidacte depuis 5ans, le piano depuis 2ans et le 
-                            développement personnel. Je également passionné de sport ou je
-                            pratique la musculation, le street workout, le tricking et le VTT</p>
+            </div>
+        </div>
+    </section>
+    <section class="sec-3">
+        <div class="container2">
+            <!--  partie gauche!-->
+            <div class="ex-Pro">
+                <h2 class="pro">Expériences professionnelles</h2>
+                <div class="experience">
+                    <div class="col-1">
+                        <div class="lieu">La Petite Provence</div>
+                        <div class="date">depuis mai 2019</div>
                     </div>
-
-                </div>
-            </div>
-        </section>
-        <section class="sec-3">
-            <div class="container2">
-                <!--  partie gauche!-->
-                <div class="ex-Pro">
-                    <h2>Expériences professionnelles</h2>  
-                    <div class="experience">         
-                        <div class="col-1">             
-                            <div class="lieu">La Petite Provence</div>         
-                            <div class="date">depuis mai 2019</div>       
-                        </div>               
-                        <div class="col-2">            
-                            <div class="profession"><br>Contrat étudiant chez un primeur</div>     
-                            <div class="details">Mise en place de la marchandise, 
-                                achats auprès de grossistes et création d'un site vitrine pour l'entreprise
-                                à l'aide d'un CMS.
-                            </div>     
-                        </div>            
-                    </div>  
-                    <div class="experience2">         
-                        <div class="col-3">                
-                            <div class="lieu">Triiptic</div>             
-                            <div class="date">2020 (7 sem.)</div>          
-                        </div>                 
-                        <div class="col-4">        
-                            <div class="profession">Stage en Développement Web </div>        
-                            <div class="details">Medley d'ajout de fonctionnalité sur un site web.
-                            </div>         
-                        </div>          
-                    </div>    
-                    <div class="experience3">         
-                        <div class="col-5">                
-                            <div class="lieu">LBC PRO</div>             
-                            <div class="date">2018 (6 sem.)</div>          
-                        </div>                 
-                        <div class="col-6">        
-                            <div class="profession">Stage en évènementiel </div>        
-                            <div class="details">Mise en place de solutions et de prestations techniques
-                                pour des événements.
-                            </div>         
-                        </div>          
-                    </div>    
-                    <div class="experience6">       
-                        <div class="col-7">            
-                            <div class="lieu">Mairie du Beausset</div>     
-                            <div class="date">2018 (6 sem.)</div> 
-                        </div>            
-                        <div class="col-8">         
-                            <div class="profession">Stage au centre informatique de la mairie</div>             
-                            <div class="details">Création du schéma réseau du Beausset, maintenant et dépannages informatiques auprès des services publics.         
-                            </div>           
-                        </div>       
-                    </div> 
-                    <div class="experience7">       
-                        <div class="col-9">            
-                            <div class="lieu">Dôme informatique</div>     
-                            <div class="date">2017(6 sem.)</div> 
-                        </div>            
-                        <div class="col-10">         
-                            <div class="profession">Stage d'informatique</div>             
-                            <div class="details">Maintenant et dépannages informatiques auprès de clients.         
-                            </div>           
-                        </div>       
+                    <div class="col-2">
+                        <div class="profession"><br>CDI chez un primeur</div>
+                        <div class="details">Mise en place de la marchandise,
+                            achats auprès de grossistes et création d'un site vitrine pour l'entreprise
+                            à l'aide d'un CMS.
+                        </div>
                     </div>
                 </div>
-                <!-- flèche centrale -->
-                <div class="fleche">
-                    <img src="assets/images/Sans titre.png" alt=""/>
+                <div class="trait_dessus">
+                    <hr>
                 </div>
-                <!--  partie droite!-->
-                <div class="ex-sco">
-                    <h2>Parcours scolaire</h2>  
-                    <div class="experience">         
-                        <div class="col-1">             
-                            <div class="lieu">La Petite Provence</div>         
-                            <div class="date">depuis mai 2019</div>       
-                        </div>               
-                        <div class="col-2">            
-                            <div class="profession"><br>Contrat étudiant chez un primeur</div>     
-                            <div class="details">Mise en place de la marchandise, 
-                                achats auprès de grossistes et création d'un site vitrine pour l'entreprise
-                                à l'aide d'un CMS.
-                            </div>     
-                        </div>            
-                    </div>  
+                <div class="experience2">
+                    <div class="col-3">
+                        <div class="lieu">Triiptic</div>
+                        <div class="date">2020 (7 sem.)</div>
+                    </div>
+                    <div class="col-4">
+                        <div class="profession"><br>Stage en Développement Web </div>
+                        <div class="details">Medley d'ajout de fonctionnalité sur un site web.
+                        </div>
+                    </div>
+                </div>
+                <div class="trait_dessus">
+                    <hr>
+                </div>
+                <div class="experience3">
+                    <div class="col-5">
+                        <div class="lieu">LBC PRO</div>
+                        <div class="date">2018 (6 sem.)</div>
+                    </div>
+                    <div class="col-6">
+                        <div class="profession"><br>Stage en évènementiel </div>
+                        <div class="details">Mise en place de solutions et de prestations techniques
+                            pour des événements.
+                        </div>
+                    </div>
+                </div>
+                <div class="trait_dessus">
+                    <hr>
+                </div>
+                <div class="experience6">
+                    <div class="col-7">
+                        <div class="lieu">Mairie du Beausset</div>
+                        <div class="date">2018 (6 sem.)</div>
+                    </div>
+                    <div class="col-8">
+                        <div class="profession"><br>Stage d'informatique</div>
+                        <div class="details">Création du schéma réseau du Beausset, maintenant et dépannages informatiques auprès des services publics.
+                        </div>
+                    </div>
+                </div>
+                <div class="trait_dessus">
+                    <hr>
+                </div>
+                <div class="experience7">
+                    <div class="col-9">
+                        <div class="lieu">Dôme informatique</div>
+                        <div class="date">2017(6 sem.)</div>
+                    </div>
+                    <div class="col-10">
+                        <div class="profession"><br>Stage d'informatique</div>
+                        <div class="details">Maintenant et dépannages informatiques auprès de clients.
+                        </div>
+                    </div>
                 </div>
             </div>
-        </section>
-    </body>
+            <!-- flèche centrale -->
+            <div class="fleche">
+                <img height="1500px" width="18px" src="assets/images/Sans titre.png" alt="" />
+            </div>
+            <!--  partie droite!-->
+            <div class="ex-sco">
+                <h2 clas="sco">Parcours scolaire</h2>
+                <div class="experience">
+                    <div class="col-1">
+                        <div class="lieu">La Petite Provence</div>
+                        <div class="date">depuis mai 2019</div>
+                    </div>
+                    <div class="col-2">
+                        <div class="profession"><br>Contrat étudiant chez un primeur</div>
+                        <div class="details">Mise en place de la marchandise,
+                            achats auprès de grossistes et création d'un site vitrine pour l'entreprise
+                            à l'aide d'un CMS.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section>
+        <center>
+            <div id="SkillBox">
+                <center>
+                    <p>My Skills</p>
+                </center>
+                <img src="https://2.bp.blogspot.com/--aPlnH6X1YA/UEf7yeJ3gGI/AAAAAAAATgw/TgMK9IVzipk/s1600/Skills%2B-%2BPawan%2BMall.png" alt="Skill - Pawan Mall" />
+                <div id="test">
+                    <div class="SkillBar">
+                        <div id="Skill-HTML">
+                            <span class="Skill-Area ">HTML</span>
+                            <span class="PercentText " id="progress_bar_html">100%</span>
+                        </div>
+                    </div>
+
+                    <div class="SkillBar">
+                        <div class="Skill-CSS">
+                            <span class="Skill-Area ">CSS/CSS3</span>
+                            <span class="PercentText ">70%</span>
+                        </div>
+                    </div>
+
+                    <div class="SkillBar">
+                        <div id="Skill-jQuery">
+                            <span class="Skill-Area ">jQuery</span>
+                            <span class="PercentText ">40%</span>
+                        </div>
+                    </div>
+
+                    <div class="SkillBar">
+                        <div id="Skill-JS">
+                            <span class="Skill-Area ">Javascript</span>
+                            <span class="PercentText ">65%</span>
+                        </div>
+                    </div>
+
+                    <div class="SkillBar">
+                        <div id="Skill-XML">
+                            <span class="Skill-Area ">XML </span>
+                            <span class="PercentText ">40%</span>
+                        </div>
+                    </div>
+
+                    <div class="SkillBar">
+                        <div id="Skill-C">
+                            <span class="Skill-Area ">C</span>
+                            <span class="PercentText ">30%</span>
+                        </div>
+                    </div>
+
+                    <div class="SkillBar">
+                        <div id="Skill-JAVA">
+                            <span class="Skill-Area ">JAVA </span>
+                            <span class="PercentText ">25%</span>
+                        </div>
+                    </div>
+
+                    <div class="SkillBar">
+                        <div id="Skill-PHP">
+                            <span class="Skill-Area ">PHP </span>
+                            <span class="PercentText ">50%</span>
+                        </div>
+                    </div>
+
+                    <div class="SkillBar">
+                        <div id="Skill-SQL">
+                            <span class="Skill-Area ">SQL </span>
+                            <span class="PercentText ">80%</span>
+                        </div>
+                    </div>
+
+                    <div class="SkillBar">
+                        <div id="Skill-VBNET">
+                            <span class="Skill-Area ">VB.NET</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </center>
+    </section>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path fill="#0099ff" fill-opacity="1" d="M0,192L24,197.3C48,203,96,213,144,224C192,235,240,245,288,234.7C336,224,384,192,432,176C480,160,528,160,576,154.7C624,149,672,139,720,154.7C768,171,816,213,864,224C912,235,960,213,1008,197.3C1056,181,1104,171,1152,176C1200,181,1248,203,1296,213.3C1344,224,1392,224,1416,224L1440,224L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path>
+    </svg>
+    <script src="js/app.js"></script>
+   
+
 </html>
-
