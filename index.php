82fa1bbc7117a05995b87999a1050ec714b806d9
leochastagnac <leochastagnac@gmail.com>
2020-12-30 14:56:12 +0100

    reponsive sec 1   2

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js"></script>
     <script src="https://unpkg.com/scrollreveal"></script>
 </head>
