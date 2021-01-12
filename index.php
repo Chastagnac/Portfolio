3f93e514fb05e165c2c7bab49d308e6ce47e1662
leochastagnac <leochastagnac@gmail.com>
2021-01-12 02:14:41 +0100

    button + responsiv

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="css/style.css" />
     <link rel="stylesheet" href="css/responsive.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/rellax/1.12.1/rellax.min.js"></script>
+    <script src="https://kit.fontawesome.com/d7d3f46c80.js" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/scrollreveal"></script>
 </head>
 
@@ -55,6 +56,21 @@
             <!--  partie gauche!-->
             <div class="ex-Pro">
                 <h2 class="pro">Expériences professionnelles</h2>
+                <div class="experience2">
+                    <div class="col-3">
+                        <div class="lieu">Triiptic</div>
+                        <div class="date">2020 (7 sem.)</div>
+                    </div>
+                    <div class="col-4">
+                        <div class="profession"><br>Stage en Développement Web </div>
+                        <div class="details">Medley d'ajout de fonctionnalité sur un site web.
+                        </div>
+                    </div>
+                </div>
+                <div class="trait_dessus">
+                    <hr>
+                </div>
+
                 <div class="experience">
                     <div class="col-1">
                         <div class="lieu">La Petite Provence</div>
@@ -65,20 +81,6 @@
                         <div class="details">Mise en place de la marchandise,
                             achats auprès de grossistes et création d'un site vitrine pour l'entreprise
                             à l'aide d'un CMS.
-                        </div>
-                    </div>
-                </div>
-                <div class="trait_dessus">
-                    <hr>
-                </div>
-                <div class="experience2">
-                    <div class="col-3">
-                        <div class="lieu">Triiptic</div>
-                        <div class="date">2020 (7 sem.)</div>
-                    </div>
-                    <div class="col-4">
-                        <div class="profession"><br>Stage en Développement Web </div>
-                        <div class="details">Medley d'ajout de fonctionnalité sur un site web.
                         </div>
                     </div>
                 </div>
@@ -135,104 +137,53 @@
                 <h2 clas="sco">Parcours scolaire</h2>
                 <div class="experience">
                     <div class="col-1">
-                        <div class="lieu">La Petite Provence</div>
-                        <div class="date">depuis mai 2019</div>
+                        <div class="lieu">BTS SIO option SLAM (en cours...)</div>
+                        <div class="date">2020</div>
                     </div>
                     <div class="col-2">
-                        <div class="profession"><br>Contrat étudiant chez un primeur</div>
-                        <div class="details">Mise en place de la marchandise,
-                            achats auprès de grossistes et création d'un site vitrine pour l'entreprise
-                            à l'aide d'un CMS.
+                        <div class="profession"><br>Etudes supérieures</div>
+                        <div class="details">Brevet de Technicien Supérieur "Services Informatiques aux
+                            Organisations", option "Solutions Logicielles et Applications
+                            Métiers"
+                        </div>
+                    </div>
+                    <div class="col-1">
+                        <div class="lieu">Bac Professionnel spécialité SN</div>
+                        <div class="date">2016</div>
+                    </div>
+                    <div class="col-2">
+                        <div class="profession"><br>Etudes supérieures</div>
+                        <div class="details">Baccalauréat Professionnel "Système Numériques"
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </section>
-    <section>
-        <center>
-            <div id="SkillBox">
-                <center>
-                    <p>My Skills</p>
-                </center>
-                <img src="https://2.bp.blogspot.com/--aPlnH6X1YA/UEf7yeJ3gGI/AAAAAAAATgw/TgMK9IVzipk/s1600/Skills%2B-%2BPawan%2BMall.png" alt="Skill - Pawan Mall" />
-                <div id="test">
-                    <div class="SkillBar">
-                        <div id="Skill-HTML">
-                            <span class="Skill-Area ">HTML</span>
-                            <span class="PercentText " id="progress_bar_html">100%</span>
-                        </div>
-                    </div>
+    <div id="sec-4">
+        <div class="container" data-rellax-speed="9">
+            <ul>
 
-                    <div class="SkillBar">
-                        <div class="Skill-CSS">
-                            <span class="Skill-Area ">CSS/CSS3</span>
-                            <span class="PercentText ">70%</span>
-                        </div>
-                    </div>
+                <li><i class="fab fa-battle-net"></i>
+                    <p>Projet</p>
+                </li>
 
-                    <div class="SkillBar">
-                        <div id="Skill-jQuery">
-                            <span class="Skill-Area ">jQuery</span>
-                            <span class="PercentText ">40%</span>
-                        </div>
-                    </div>
+                <li><i class="fas fa-stopwatch-20" data-rellax-speed="7"></i>
+                    <p>Veille</p>
+                </li>
 
-                    <div class="SkillBar">
-                        <div id="Skill-JS">
-                            <span class="Skill-Area ">Javascript</span>
-                            <span class="PercentText ">65%</span>
-                        </div>
-                    </div>
+                <li><i class="fas fa-envelope-open-text" data-rellax-speed="7"></i>
+                    <p>Contact</p>
+                </li>
 
-                    <div class="SkillBar">
-                        <div id="Skill-XML">
-                            <span class="Skill-Area ">XML </span>
-                            <span class="PercentText ">40%</span>
-                        </div>
-                    </div>
+            </ul>
+        </div>
+    </div>
+    <footer>
 
-                    <div class="SkillBar">
-                        <div id="Skill-C">
-                            <span class="Skill-Area ">C</span>
-                            <span class="PercentText ">30%</span>
-                        </div>
-                    </div>
+    </footer>
 
-                    <div class="SkillBar">
-                        <div id="Skill-JAVA">
-                            <span class="Skill-Area ">JAVA </span>
-                            <span class="PercentText ">25%</span>
-                        </div>
-                    </div>
-
-                    <div class="SkillBar">
-                        <div id="Skill-PHP">
-                            <span class="Skill-Area ">PHP </span>
-                            <span class="PercentText ">50%</span>
-                        </div>
-                    </div>
-
-                    <div class="SkillBar">
-                        <div id="Skill-SQL">
-                            <span class="Skill-Area ">SQL </span>
-                            <span class="PercentText ">80%</span>
-                        </div>
-                    </div>
-
-                    <div class="SkillBar">
-                        <div id="Skill-VBNET">
-                            <span class="Skill-Area ">VB.NET</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </center>
-    </section>
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path fill="#0099ff" fill-opacity="1" d="M0,192L24,197.3C48,203,96,213,144,224C192,235,240,245,288,234.7C336,224,384,192,432,176C480,160,528,160,576,154.7C624,149,672,139,720,154.7C768,171,816,213,864,224C912,235,960,213,1008,197.3C1056,181,1104,171,1152,176C1200,181,1248,203,1296,213.3C1344,224,1392,224,1416,224L1440,224L1440,320L1416,320C1392,320,1344,320,1296,320C1248,320,1200,320,1152,320C1104,320,1056,320,1008,320C960,320,912,320,864,320C816,320,768,320,720,320C672,320,624,320,576,320C528,320,480,320,432,320C384,320,336,320,288,320C240,320,192,320,144,320C96,320,48,320,24,320L0,320Z"></path>
-    </svg>
     <script src="js/app.js"></script>
-   
+
 
 </html>
